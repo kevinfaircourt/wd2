e5809b76da48b709a1197e74733588b40d490ed9
kevinfaircourt <61220815+kevinfaircourt@users.noreply.github.com>
2020-05-05 22:40:39 -0700

    quokka/header change demo
    in class demo, image swap between quokka images, change header color to random.

---
+++ week10/script.js
@@ -0,0 +1,35 @@
+var header = document.getElementById("docHeader");
+var hColorChangeBtn = document.getElementById("ChangeColor");
+
+var quokkaImage = document.getElementById("quokkaImg");
+var quokkaButton = document.getElementById("quokkaImgChange");
+
+var headerColorChange = function(){
+var redComp = Math.random() *255;
+var greenComp = Math.random() *255;
+var blueComp = Math.random() *255;
+
+header.style.backgroundColor = "rgb(" + redComp + ", " + greenComp + ", " + blueComp + ")";
+
+}
+
+var swapQuokkaImage = function(){
+    console.log(quokkaImage.alt);
+
+    if(quokkaImage.alt == "First quokka image"){
+        quokkaImage.src = "images/quok.jpg";
+        quokkaImage.alt = "Second quokka image";
+        quokkaButton.innerText = "Show first one again";
+    }
+    else { 
+        quokkaImage.src = "images/quok1.jpg";
+        quokkaImage.alt = "Second quokka image";
+        quokkaButton.innerText = "Which one is better?";
+    }
+}
+
+//just when the JS loads
+headerColorChange();
+
+hColorChangeBtn.addEventListener("click" , headerColorChange);
+quokkaButton.addEventListener("click", swapQuokkaImage);
