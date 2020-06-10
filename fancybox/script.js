874e969af0a2bc22377b58c1ae525c6187dd8c62
kevinfaircourt <61220815+kevinfaircourt@users.noreply.github.com>
2020-06-10 15:56:02 -0700

    js needs edit
    thank you screen, show and hide js work, but not the others.

--- fancybox/script.js
+++ fancybox/script.js
@@ -2,4 +2,25 @@ $(document).ready(function(){
     $(".toggle-button").click(function(){
         $(".nav-links").slideToggle();
     });
+    // Replace form with replace message
+  $(".donate-submit").click(function () {
+    $("form").replaceWith($("#thanks"));
+  });
+  $("#donationTop").click(function(){
+    $("#thanks").show();
+  });
+  $("#donationTop").click(function(){
+    $("form").hide();
+  });
+  //   Remove hidden class from replace message
+  $(".donate-submit").click(function () {
+    $(".replace").removeClass("hidden-all");
+  });
+
+  $('form').submit(function (event) {
+    event.preventDefault();
+  });
+
+  // Go to top of donation
+  $('#donationTop').click(function(){ window.location = '#thanks'; });
 });
