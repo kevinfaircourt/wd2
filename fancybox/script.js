6f454fff0e1b937566386502740253b4f08da15b
kevinfaircourt <61220815+kevinfaircourt@users.noreply.github.com>
2020-06-11 04:25:09 -0700

    added droppable, not working
    error with js link i guess, also maybe curly brack or semi colon is off.

--- fancybox/script.js
+++ fancybox/script.js
@@ -2,6 +2,25 @@ $(document).ready(function(){
     $(".toggle-button").click(function(){
         $(".nav-links").slideToggle();
     });
+
+    var numProducts = 0;
+
+    $(".draggable").draggable();
+  
+    $(".droppable").droppable({
+        drop: function (event, ui) {
+            numProducts++;
+            var plural = '';
+            if(numProducts > 1 ){
+                plural = "s";
+            }
+            $(this)
+                .find("h3")
+                .html("You helped " + numProducts + " Quokka" + plural + "get to the forest!");
+        }
+    });
+
+
     // Replace form with replace message
   $(".donate-submit").click(function () {
     $("form").replaceWith($("#thanks"));
@@ -30,5 +49,11 @@ $(document).ready(function(){
   });
 
   // Go to top of donation
-  $('#donationTop').click(function(){ window.location = '#thanks'; });
+  $('#donationTop').click(function(){ window.location = '#thanks'; 
+});
+
+ 
+
+
+
 });
